3193eced9bad7e76fafbca8a2d8f93ff2e2d70f2
Adam Nowak <adam.nowak@netguru.pl>
2013-03-08 19:07:36 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Easyblog::Application.config.secret_token = 'c3049aa7654be3fa2bf7e2acd3047a285535b05283eda9f57c4ffc17637b2ee1f8941b85077f4c86b2fc7b9352921ff3709296563258fcb63dd653b8852341ca'
