632697a670ffa103d55cb39b8ed04837c8bf3db6
Adam Nowak <adam.nowak@netguru.pl>
2013-05-09 21:57:04 +0200

    display notifications

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :display_flash_notice
+
+  private
+
+  def display_flash_notice
+    flash[:notice] = "this is a test project for netguru"
+  end
+
 end
