bc033664bd5c9c782a72227771605085c2af272f
Tomasz Pewiński <pewniak747@gmail.com>
2013-05-12 17:00:43 +0200

    grammar nazi on duty

--- spec/decorators/post_decorator_spec.rb
+++ spec/decorators/post_decorator_spec.rb
@@ -4,7 +4,7 @@ describe PostDecorator do
   let(:decorated_post) { PostDecorator.new(title: "Game of Thrones III") }
 
   describe ".friendly_title" do
-    it "returns title where words are downcaced and separated by '-' sign" do
+    it "returns title where words are downcased and separated by '-' sign" do
       decorated_post.friendly_title.should eq "game-of-thrones-iii"
     end
   end
