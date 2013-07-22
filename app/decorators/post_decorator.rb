c3f961787480fad1eb501839c906e14ab24d7a1a
Dawid Woźniak <dawid.wozniak@netguru.pl>
2013-07-23 00:13:10 +0200

    add method to post decorator

--- app/decorators/post_decorator.rb
+++ app/decorators/post_decorator.rb
@@ -5,4 +5,8 @@ class PostDecorator < Draper::Decorator
   def friendly_title
     title.gsub(' ', '-').downcase
   end
+
+  def truncated_body
+    h.raw h.truncate(body, length: 200, omission: "...")
+  end
 end
