f5ac9fd8b0a3ace4c3db9a815183c03d25dff8a9
Dawid Woźniak <dawid.wozniak@netguru.pl>
2013-07-22 20:29:06 +0200

    fix posts_controller_spec.rb:12

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :display_flash_notice
-
-  private
-
-  def display_flash_notice
-    flash[:notice] = "this is a test project for netguru"
-  end
 
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
+  before_filter :display_flash_notice
+
   def index
     @users = User.all
+  end
+
+  private
+
+  def display_flash_notice
+    flash[:notice] = "this is a test project for netguru"
   end
 end
