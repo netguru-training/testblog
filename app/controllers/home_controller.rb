7bd7ff54b744a230f1ab67e21bcf0b66be888d26
Dawid Woźniak <dawid.wozniak@netguru.pl>
2013-07-22 20:50:54 +0200

    small changes to the controllers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,5 @@
 class HomeController < ApplicationController
-  before_filter :display_flash_notice
-
   def index
     @users = User.all
-  end
-
-  private
-
-  def display_flash_notice
-    flash[:notice] = "this is a test project for netguru"
   end
 end
