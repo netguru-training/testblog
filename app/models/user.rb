85aac488e830949cae4b02f93ab61e7d9a1c68e4
Dawid Woźniak <dawid.wozniak@netguru.pl>
2013-07-22 20:37:04 +0200

    fix for user_spec.rb:4

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,9 +29,10 @@ class User
 
   index({ email: 1 }, { unique: true, background: true })
   validates_presence_of :name
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :created_at, :updated_at
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :created_at, :updated_at, :nickname
 
   field :name, type: String
+  field :nickname, type: String
 
   def owner? post
     posts.include? post
