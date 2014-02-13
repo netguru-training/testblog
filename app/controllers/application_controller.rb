a89dec14928a2fcbf3fe1a0b48fec45a3e00cb19
Przemysław Dąbek <przemek.dabek@getbase.com>
2014-02-13 21:16:45 +0100

    allow :name, :nickname in strong parameters for devise account_update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,5 +12,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:account_update) << :name
+    devise_parameter_sanitizer.for(:account_update) << :nickname
   end
 end
